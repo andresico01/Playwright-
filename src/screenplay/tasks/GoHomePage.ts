87e10035e989644a1b01bd139846c16d80ca9806
andresico01 <andresrojasdg2.0@gmail.com>
2026-02-22 02:06:23 -0500

    add AzureCLI task

--- src/screenplay/tasks/GoHomePage.ts
+++ src/screenplay/tasks/GoHomePage.ts
@@ -3,5 +3,6 @@ import { Actor } from "@actors/Actor";
 export const BrowseLolPage = {
     goToHomePage: () => async (actor: Actor) => {
         await actor.page.goto('/');
+        console.log(actor.page.url());
     }
 };
